cee1846912f01012e8cddbc58ee9228539201a08
Lucas Otoni <lotoni.vivo@gmail.com>
2022-07-14 17:14:47 -0300

    requisito 6 100%

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { cleanup, render, screen, waitFor, act, fireEvent } from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
-import renderPath from './renderPath';
+import renderWithRouter from './renderWithRouter';
 
 describe('test the login page', () => {
-    beforeEach(async () => {
-        await act(async () =>{
-          renderPath("/")
-        } )
-    });
+    // beforeEach(async () => {
+    //     await act(async () =>{
+    //       renderPath("/")
+    //     } )
+    // });
 
     test('test if form button is able when text inputs are filled correctly', () => {
+      renderWithRouter(<App />)
       const emailInput = screen.getByTestId("email-input");
       const passwordInput = screen.getByTestId("password-input");
       const formBtn = screen.getByTestId("login-submit-btn");
@@ -23,6 +24,7 @@ describe('test the login page', () => {
     });
 
     test('test if form button is disabled when text inputs are filled incorrectly', () => {
+      renderWithRouter(<App />)
       const emailInput = screen.getByTestId("email-input");
       const passwordInput = screen.getByTestId("password-input");
       const formBtn = screen.getByTestId("login-submit-btn");
@@ -33,4 +35,17 @@ describe('test the login page', () => {
       expect(formBtn).toBeDisabled();
     });
     
+    test('test if u get redirected when click the form button', () => {
+      const { history } = renderWithRouter(<App />)
+      const emailInput = screen.getByTestId("email-input");
+      const passwordInput = screen.getByTestId("password-input");
+      const formBtn = screen.getByTestId("login-submit-btn");
+
+
+      userEvent.type(emailInput, 'lucas@lucas.com');
+      userEvent.type(passwordInput, '12345678');
+      userEvent.click(formBtn);
+      
+      expect(history.location.pathname).toBe('/foods');
+    });
 })
