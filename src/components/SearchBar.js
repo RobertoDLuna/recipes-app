60562f2a4951b905dd9ab9765cdd5c45842b11e2
Lucas Otoni <lotoni.vivo@gmail.com>
2022-07-15 15:19:49 -0300

    requisito 11 100%

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,60 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { DataContext } from '../context/DataContext';
 
 function SearchBar() {
+  const { setFoodFilter, changeSearch } = useContext(DataContext);
+  const [type, setType] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const handleClick = () => {
+    changeSearch(type);
+    setFoodFilter(filter);
+  };
+
   return (
     <div>
-      <input type="text" data-testid="search-input" placeholder="Buscar" />
-      <label htmlFor="search">
+      <input
+        type="text"
+        data-testid="search-input"
+        placeholder="Buscar"
+        onChange={ ({ target: { value } }) => setType(value) }
+      />
+      <label htmlFor="ingredient">
         <input
           type="radio"
-          name="search"
-          id="search"
+          name="filter"
+          id="ingredient"
+          value="ingredient"
           data-testid="ingredient-search-radio"
+          onClick={ ({ target: { value } }) => setFilter(value) }
         />
         Ingrediente
       </label>
       <label htmlFor="name">
         <input
           type="radio"
-          name="name"
+          name="filter"
+          value="name"
           id="name"
           data-testid="name-search-radio"
+          onClick={ ({ target: { value } }) => setFilter(value) }
         />
         Nome
       </label>
       <label htmlFor="first">
         <input
           type="radio"
-          name="first"
+          name="filter"
           id="first"
+          value="firstLetter"
           data-testid="first-letter-search-radio"
+          onClick={ ({ target: { value } }) => setFilter(value) }
         />
         Primeira Letra
       </label>
-      <button type="button" data-testid="exec-search-btn">Buscar</button>
+      <button type="button" data-testid="exec-search-btn" onClick={ () => handleClick() }>
+        Buscar
+      </button>
     </div>
   );
 }
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const DataContext = createContext();
 
@@ -13,6 +13,37 @@ function DataProvider({ children }) {
   const [mealsToken, setMealsToken] = useState(1);
   const [cocktailsToken, setCocktailsToken] = useState(1);
 
+  const [searched, setSearched] = useState('');
+  const changeSearch = (param) => {
+    setSearched(param);
+  };
+  const [foodFilter, setFoodFilter] = useState('');
+  const [recipes, setRecipes] = useState([]);
+  const changeRecipes = (param) => {
+    setRecipes(param);
+  };
+
+  useEffect(() => {
+    const doFetch = async () => {
+      if (foodFilter === 'ingredient') {
+        const resp = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searched}`);
+        const json = await resp.json();
+        changeRecipes(json);
+      } else if (foodFilter === 'name') {
+        const resp = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searched}`);
+        const json = await resp.json();
+        changeRecipes(json);
+      } else if (foodFilter === 'firstLetter' && searched.length > 1) {
+        global.alert('Your search must have only 1 (one) character');
+      } else if (foodFilter === 'firstLetter') {
+        const resp = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searched}`);
+        const json = await resp.json();
+        changeRecipes(json);
+      }
+    };
+    doFetch();
+  }, [searched, foodFilter]);
+
   const contextValue = {
     userData,
     setUserData,
@@ -20,6 +51,11 @@ function DataProvider({ children }) {
     mealsToken,
     setCocktailsToken,
     setMealsToken,
+    searched,
+    changeSearch,
+    setFoodFilter,
+    foodFilter,
+    recipes,
   };
 
   return (
