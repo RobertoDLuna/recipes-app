9c66b2ccfbca40c88a28b7db98e1148f5406b47e
Lucas Otoni <lotoni.vivo@gmail.com>
2022-07-15 16:43:46 -0300

    requisito 13 100%

--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,14 +1,28 @@
 // import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
-// import { DataContext } from '../context/DataContext';
+import { DataContext } from '../context/DataContext';
 
-function Foods() {
+function Foods({ history }) {
+  const { recipes } = useContext(DataContext);
+  useEffect(() => {
+    const recipe = Object.values(recipes);
+    if (recipe && recipe.length === 1) {
+      history.push(`/foods/${recipe[0][0].idMeal}`);
+    }
+  }, [recipes.length, history, recipes]);
   return (
     <div>
       <Header title="Foods" bool btnName="food" />
     </div>
   );
 }
+
+Foods.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Foods;
