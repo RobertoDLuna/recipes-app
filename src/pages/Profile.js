48fa6823fd5ec1ac44e697ee7c38c0186d2b28a3
Otavio RL <otaviohlopess@gmail.com>
2022-07-18 18:57:23 -0300

    57 a 61

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,35 @@
 // import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 // import { DataContext } from '../context/DataContext';
 
 function Profile() {
+  const string = localStorage.getItem('user');
+  const { email } = JSON.parse(string);
+  const handleClick = () => {
+    localStorage.clear();
+  };
   return (
     <div>
       <Header title="Profile" bool={ false } />
-      <h3 data-testid="profile-email">Email</h3>
-      <button data-testid="profile-done-btn" type="button">Done Recipes</button>
-      <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
-      <button data-testid="profile-logout-btn" type="button">Logout</button>
+      <h3 data-testid="profile-email">{ email }</h3>
+      <Link to="/done-recipes">
+        <button data-testid="profile-done-btn" type="button">Done Recipes</button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
+      </Link>
+      <Link to="/">
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ handleClick }
+        >
+          Logout
+        </button>
+      </Link>
       <Footer />
     </div>
   );
