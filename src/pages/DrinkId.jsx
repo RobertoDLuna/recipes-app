a2382fff5e4e8e9e251e66efd2abea749f40bea1
Lucas Otoni <lotoni.vivo@gmail.com>
2022-07-18 16:53:22 -0300

    requisito 24 100%

--- src/pages/DrinkId.jsx
+++ src/pages/DrinkId.jsx
@@ -1,8 +1,21 @@
 // import PropTypes from 'prop-types';
-import React from 'react';
-// import { DataContext } from '../context/DataContext';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { DataContext } from '../context/DataContext';
 
 function DrinkId() {
+  const { setFilteredById } = useContext(DataContext);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const resp = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const json = await resp.json();
+      setFilteredById(json);
+    };
+    doFetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div>
       <h1>oi</h1>
