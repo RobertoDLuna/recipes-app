0ad38fad4d5572e09fb44ffdb4025f409e2c8003
Lucas Otoni <lotoni.vivo@gmail.com>
2022-07-18 15:00:15 -0300

    requisito 21

--- src/components/DrinkFilteredRecipes.js
+++ src/components/DrinkFilteredRecipes.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { DataContext } from '../context/DataContext';
 
 function DrinkFilteredRecipes() {
   const [categories, setCategories] = useState([]);
+  const { changeRecipes, allButtonState } = useContext(DataContext);
 
   useEffect(() => {
     const categoryFetch = async () => {
@@ -12,19 +14,34 @@ function DrinkFilteredRecipes() {
     categoryFetch();
   }, []);
 
+  const fetchByButtons = async (param) => {
+    const resp = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${param}`);
+    const json = await resp.json();
+    changeRecipes(json);
+  };
+
   return (
     <div>
       {categories
       && categories.map((e, index) => (
         <div key={ index }>
           <button
+            name={ e.strCategory }
             type="button"
             data-testid={ `${e.strCategory}-category-filter` }
+            onClick={ ({ target }) => fetchByButtons(target.name) }
           >
             {e.strCategory}
           </button>
         </div>
       ))}
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        onClick={ () => changeRecipes(allButtonState) }
+      >
+        All
+      </button>
     </div>
   );
 }
--- src/components/FoodFilteredRecipes.js
+++ src/components/FoodFilteredRecipes.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { DataContext } from '../context/DataContext';
 
 function FoodFilteredRecipes() {
   const [categories, setCategories] = useState([]);
+  const { changeRecipes, allButtonState, setGoatTrue } = useContext(DataContext);
 
   useEffect(() => {
     const categoryFetch = async () => {
@@ -12,19 +14,37 @@ function FoodFilteredRecipes() {
     categoryFetch();
   }, []);
 
+  const fetchByButtons = async (param) => {
+    const resp = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${param}`);
+    const json = await resp.json();
+    if (param === 'Goat') {
+      setGoatTrue(true);
+    }
+    changeRecipes(json);
+  };
+
   return (
     <div>
       {categories
       && categories.map((e, index) => (
         <div key={ index }>
           <button
+            name={ e.strCategory }
             type="button"
             data-testid={ `${e.strCategory}-category-filter` }
+            onClick={ ({ target }) => fetchByButtons(target.name) }
           >
             {e.strCategory}
           </button>
         </div>
       ))}
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        onClick={ () => changeRecipes(allButtonState) }
+      >
+        All
+      </button>
     </div>
   );
 }
